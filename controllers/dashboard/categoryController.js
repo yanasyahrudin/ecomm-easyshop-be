5af57ea06c83818d5b1c7be8c627eaea44c567ab
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-08-03 06:38:12 +0700

    setup category add and get part 4

--- controllers/dashboard/categoryController.js
+++ controllers/dashboard/categoryController.js
@@ -1,10 +1,24 @@
+const { responseReturn } = require("../../utiles/response");
+const formidable = require("formidable");
 
-
-class categoryControllers{
-
-    add_category = async(req,res) => {
-        console.log('this is working');
-    }
+class categoryControllers {
+    
+  add_category = async (req, res) => {
+    const form = formidable();
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        responseReturn(res, 404, { error: "something went wrong" });
+      }else {
+        let {name} = fields
+        let {image} = files
+        name = name.trim()
+        const slug = name.split(' ').join('-')
+      }
+    });
+  };
+  get_category = async (req, res) => {
+    console.log("this is working");
+  };
 }
 
-module.exports = new categoryControllers()
+module.exports = new categoryControllers();
