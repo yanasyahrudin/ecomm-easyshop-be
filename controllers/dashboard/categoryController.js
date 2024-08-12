19eb78c3f8439e54683bb93bcb575543302e026b
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-08-13 06:42:07 +0700

    setup category add and get part 8

--- controllers/dashboard/categoryController.js
+++ controllers/dashboard/categoryController.js
@@ -1,53 +1,91 @@
-const { responseReturn } = require("../../utiles/response");
-const formidable = require("formidable");
-const cloudinary = require("cloudinary").v2;
-const categoryModel = require("../../models/categoryModel");
+const formidable = require("formidable")
+const { responseReturn } = require("../../utiles/response")
+const cloudinary = require('cloudinary').v2
+const categoryModel = require('../../models/categoryModel')
 
-class categoryControllers {
-  add_category = async (req, res) => {
-    const form = formidable();
-    form.parse(req, async (err, fields, files) => {
-      if (err) {
-        responseReturn(res, 404, { error: "Something Went Wrong" });
-      } else {
-        let { name } = fields;
-        let { image } = files;
-        name = name.trim();
-        const slug = name.split(" ").join("-");
+class categoryController{
 
-        cloudinary.config({
-          cloud_name: process.env.cloud_name,
-          api_key: process.env.api_key,
-          api_secret: process.env.api_secret,
-          secure: true,
-        });
+    add_category = async (req, res) => {
+        const form = formidable()
+        form.parse(req, async(err,fields,files)=>{
+            if (err) {
+                responseReturn(res, 404,{ error : 'something went wrong'})
+            } else {
+                let {name} = fields
+                let {image} = files
+                name = name.trim()
+                const slug = name.split(' ').join('-')
 
-        try {
-          const result = await cloudinary.uploader.upload(image.filepath, {
-            folder: "categorys",
-          });
-          if (result) {
-            const category = await categoryModel.create({
-              name,
-              slug,
-              image: result.url,
-            });
-            responseReturn(res, 201, {
-              category,
-              message: "Category Added Successfully",
-            });
-          } else {
-            responseReturn(res, 404, { error: "Image Upload File" });
-          }
-        } catch (error) {
-          responseReturn(res, 500, { error: "Internet Server Error" });
+                cloudinary.config({
+                    cloud_name: process.env.cloud_name,
+                    api_key: process.env.api_key,
+                    api_secret: process.env.api_secret,
+                    secure: true
+                })
+
+               try {
+
+                const result = await cloudinary.uploader.upload(image.filepath, { folder: 'categorys'})
+
+                if (result) {
+                    const category = await categoryModel.create({
+                        name,
+                        slug,
+                        image: result.url
+                    })
+                    responseReturn(res, 201,{ category,message : 'Category Added Successfully'})
+                    
+                } else {
+                    responseReturn(res, 404,{ error : 'Image Upload File'})
+                }
+                
+               } catch (error) {
+                responseReturn(res, 500,{ error : 'Internal Server Error'})
+               }
+ 
+
+
+            }
+             
+        })
+    }
+
+ // end method
+
+    get_category = async (req, res) => {
+       const {page,searchValue, parPage} = req.query
+       const skipPage = parseInt(parPage) * (parseInt(page) - 1)
+
+       try {
+        if (searchValue) {
+            const categorys = await categoryModel.find({
+                $text: { $search: searchValue }
+            }).skip(skipPage).limit(parPage).sort({ createdAt: -1})
+            const totalCategory = await categoryModel.find({
+                $text: { $search: searchValue }
+            }).countDocuments()
+            responseReturn(res, 200,{categorys,totalCategory})
+        } else {
+
+            const categorys = await categoryModel.find({ }).skip(skipPage).limit(parPage).sort({ createdAt: -1})
+            const totalCategory = await categoryModel.find({ }).countDocuments()
+            responseReturn(res, 200,{categorys,totalCategory})
+            
         }
-      }
-    });
-  };
-  get_category = async (req, res) => {
-    const { page, searchValue, parPage } = req.query;
-  };
-}
+        
+       } catch (error) {
+        
+       }
 
-module.exports = new categoryControllers();
+
+    }
+
+    // end method 
+
+
+
+
+}
+ 
+
+module.exports = new categoryController()
