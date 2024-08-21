b60205e4c40ca2a6ec84995edb2c4de7df24149d
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-08-22 06:20:10 +0700

    add product setup part 6

--- controllers/dashboard/productController.js
+++ controllers/dashboard/productController.js
@@ -1,19 +1,51 @@
-const formidable = require("formidable")
-const { responseReturn } = require("../../utiles/response")
-const cloudinary = require('cloudinary').v2
+const formidable = require("formidable");
+const { responseReturn } = require("../../utiles/response");
+const cloudinary = require("cloudinary").v2;
+const productModel = require('../../models/productModel')
 
+class productController {
+  add_product = async (req, res) => {
+    const form = formidable({ multiples: true });
 
-class productController{
-    add_product = async(req,res)=>{
-        const form = formidable({multiples:true})
+    form.parse(req, async (err, field, files) => {
+      let {
+        name,
+        category,
+        description,
+        stock,
+        price,
+        discount,
+        shopName,
+        brand,
+      } = field;
+      const { images } = files;
+      name = name.trim();
+      const slug = name.split(" ").join("-");
 
-        form.parse(req,async(err,field,files)=>{
-            console.log(files.images[0]);
-            console.log(field);
+      cloudinary.config({
+        cloud_name: process.env.cloud_name,
+        api_key: process.env.api_key,
+        api_secret: process.env.api_secret,
+        secure: true,
+      });
+
+      try {
+        let allImageUrl = [];
+        for (let i = 0; i < images.length; i++) {
+          const result = await cloudinary.uploader.upload(images[i].filepath, {
+            folder: "products",
+          });
+          allImageUrl = [...allImageUrl, result.url];
+        }
+
+        await productModel.create({
             
         })
-        
-    }
+      } catch (error) {}
+    });
+  };
+
+  //end method
 }
 
-module.exports = new productController()
+module.exports = new productController();
