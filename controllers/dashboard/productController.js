9f9b9f38b52302879263675f4ca8da2671e16d05
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-09-11 06:45:32 +0700

    product image change part 2

--- controllers/dashboard/productController.js
+++ controllers/dashboard/productController.js
@@ -115,31 +115,73 @@ class productController {
   ///End method
 
   product_update = async (req, res) => {
-    let {name, description, stock,price, discount,brand,productId} = req.body;
-    name = name.trim()
-    const slug = name.split(' ').join('-')
+    let { name, description, stock, price, discount, brand, productId } =
+      req.body;
+    name = name.trim();
+    const slug = name.split(" ").join("-");
 
     try {
-        await productModel.findByIdAndUpdate(productId, {
-            name, description, stock,price, discount,brand,productId, slug
-        })
-        const product = await productModel.findById(productId)
-        responseReturn(res, 200,{product, message : 'Product Updated Successfully'})
+      await productModel.findByIdAndUpdate(productId, {
+        name,
+        description,
+        stock,
+        price,
+        discount,
+        brand,
+        productId,
+        slug,
+      });
+      const product = await productModel.findById(productId);
+      responseReturn(res, 200, {
+        product,
+        message: "Product Updated Successfully",
+      });
     } catch (error) {
-        responseReturn(res, 500,{ error : error.message })
-    } 
-}
+      responseReturn(res, 500, { error: error.message });
+    }
+  };
 
   // end method
-  
-  product_image_update = async(req, res)=>{
-    const form =formidable({multiples: true})
-    form.parse(req, (err,field, files)=>{
-      console.log(field);
-      console.log(files);
-      
-    })
-  }
+
+  product_image_update = async (req, res) => {
+    const form = formidable({ multiples: true });
+
+    form.parse(req, async (err, field, files) => {
+      const { oldImage, productId } = field;
+      const { newImage } = files;
+
+      if (err) {
+        responseReturn(res, 400, { error: err.message });
+      } else {
+        try {
+          cloudinary.config({
+            cloud_name: process.env.cloud_name,
+            api_key: process.env.api_key,
+            api_secret: process.env.api_secret,
+            secure: true,
+          });
+          const result = await cloudinary.uploader.upload(newImage.filepath, {
+            folder: "products",
+          });
+          if (result) {
+            let { images } = await productModel.findById(productId);
+            const index = images.findIndex((img) => img === oldImage);
+            images[index] = result.url;
+            await productModel.findByIdAndUpdate(productId, { images });
+            const product = await productModel.findById(productId);
+            responseReturn(res, 200, {
+              product,
+              message: "Product Image Successfully",
+            });
+          } else {
+            responseReturn(res, 400, { error: "Image Upload Failed" });
+          }
+        } catch (error) {
+          responseReturn(res, 404, { error: error.message });
+        }
+      }
+    });
+  };
   // end method
 }
 
