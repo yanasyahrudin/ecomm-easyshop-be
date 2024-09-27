e097306806b35845c64653036dbf74278c11734b
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-09-28 06:26:17 +0700

    seller details and status manage part 1

--- controllers/dashboard/sellerController.js
+++ controllers/dashboard/sellerController.js
@@ -1,34 +1,45 @@
-const formidable = require("formidable")
-const { responseReturn } = require("../../utiles/response")
-const cloudinary = require('cloudinary').v2
-const sellerModel = require('../../models/sellerModel')
+const formidable = require("formidable");
+const { responseReturn } = require("../../utiles/response");
+const cloudinary = require("cloudinary").v2;
+const sellerModel = require("../../models/sellerModel");
 
-class sellerController{
+class sellerController {
+  request_seller_get = async (req, res) => {
+    const { page, searchValue, parPage } = req.query;
+    const skipPage = parseInt(parPage) * (parseInt(page) - 1);
 
-    request_seller_get = async (req,res)=>{
-        const {page,searchValue, parPage} = req.query
-        const skipPage = parseInt(parPage) * (parseInt(page) - 1)
-        
-        try {
-            if (searchValue) {
-                
-            } else {
-                const sellers = await sellerModel.find({
-                    status: 'pending'
-                }).skip(skipPage).limit(parPage).sort({ createdAt: -1})
-                const totalSeller = await sellerModel.find({ status: 'pending'}).countDocuments()
-                responseReturn(res, 200,{sellers,totalSeller})
-            }
-        } catch (error) {
-            responseReturn(res, 500,{error:error.message})
-        }
+    try {
+      if (searchValue) {
+      } else {
+        const sellers = await sellerModel
+          .find({
+            status: "pending",
+          })
+          .skip(skipPage)
+          .limit(parPage)
+          .sort({ createdAt: -1 });
+        const totalSeller = await sellerModel
+          .find({ status: "pending" })
+          .countDocuments();
+        responseReturn(res, 200, { sellers, totalSeller });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
     }
+  };
 
-    //end method
+  //end method
 
-
+  get_seller = async (req, res) => {
+    const { sellerId } = req.params;
+    try {
+      const seller = await sellerModel.findById(sellerId);
+      responseReturn(res, 200, { seller });
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
+    }
+  };
 
 }
- 
 
-module.exports = new sellerController()
+module.exports = new sellerController();
