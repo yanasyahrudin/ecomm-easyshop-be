38a415f3f9f0c7c7aebdd9696f4eb6ecb20c8248
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-10-02 06:55:26 +0700

    seller details and status manage part 3

--- controllers/dashboard/sellerController.js
+++ controllers/dashboard/sellerController.js
@@ -1,45 +1,60 @@
-const formidable = require("formidable");
-const { responseReturn } = require("../../utiles/response");
-const cloudinary = require("cloudinary").v2;
-const sellerModel = require("../../models/sellerModel");
+const formidable = require("formidable")
+const { responseReturn } = require("../../utiles/response")
+const cloudinary = require('cloudinary').v2
+const sellerModel = require('../../models/sellerModel')
 
-class sellerController {
-  request_seller_get = async (req, res) => {
-    const { page, searchValue, parPage } = req.query;
-    const skipPage = parseInt(parPage) * (parseInt(page) - 1);
+class sellerController{ 
 
-    try {
-      if (searchValue) {
-      } else {
-        const sellers = await sellerModel
-          .find({
-            status: "pending",
-          })
-          .skip(skipPage)
-          .limit(parPage)
-          .sort({ createdAt: -1 });
-        const totalSeller = await sellerModel
-          .find({ status: "pending" })
-          .countDocuments();
-        responseReturn(res, 200, { sellers, totalSeller });
-      }
-    } catch (error) {
-      responseReturn(res, 500, { error: error.message });
+    request_seller_get = async (req, res) => {
+        const {page,searchValue, parPage} = req.query 
+        const skipPage = parseInt(parPage) * (parseInt(page) - 1)
+
+        try {
+            if (searchValue) {
+                
+            } else {
+                const sellers = await sellerModel.find({ status:  'pending'}).skip(skipPage).limit(parPage).sort({ createdAt: -1})
+                const totalSeller = await sellerModel.find({ status: 'pending' }).countDocuments()
+                responseReturn(res, 200,{ sellers,totalSeller })
+            }
+        } catch (error) {
+            responseReturn(res, 500,{ error: error.message }) 
+        }
+ 
     }
-  };
 
-  //end method
+    
+    // end method 
 
-  get_seller = async (req, res) => {
-    const { sellerId } = req.params;
-    try {
-      const seller = await sellerModel.findById(sellerId);
-      responseReturn(res, 200, { seller });
-    } catch (error) {
-      responseReturn(res, 500, { error: error.message });
+    get_seller = async (req, res) => {
+        const {sellerId} = req.params
+        try {
+            const seller = await sellerModel.findById(sellerId)
+            responseReturn(res, 200,{ seller })
+        } catch (error) {
+            responseReturn(res, 500,{ error: error.message })
+        }
     }
-  };
+
+     // end method 
+
+     seller_status_update = async (req, res) => {
+        const {sellerId, status} = req.body
+        try {
+            await sellerModel.findByIdAndUpdate(sellerId,{status})
+            const seller = await sellerModel.findById(sellerId)
+            responseReturn(res, 200,{ seller,  message: 'Seller Status Updated Successfully' })
+        } catch (error) {
+            responseReturn(res, 500,{ error: error.message })
+        }
+    }
+
+     // end method 
+
+
+
 
 }
+ 
 
-module.exports = new sellerController();
+module.exports = new sellerController()
--- routes/dashboard/sellerRoutes.js
+++ routes/dashboard/sellerRoutes.js
@@ -3,6 +3,7 @@ const { authMiddleware } = require('../../middlewares/authMiddleware')
 const router = require('express').Router()
 
 router.get('/request-seller-get', authMiddleware,sellerController.request_seller_get)
-router.get('/get-seller/:selerId', authMiddleware,sellerController.get_seller)
+router.get('/get-seller/:sellerId', authMiddleware,sellerController.get_seller)
+router.post('/seller-status-update', authMiddleware,sellerController.seller_status_update)
 
 module.exports = router
