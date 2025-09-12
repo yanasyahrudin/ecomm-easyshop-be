f40d3cfec5a386f010baa2da33ed46c14493b644
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-12 13:26:01 +0700

    fetch feature product and latest top rated discount part 1

--- controllers/home/homeController.js
+++ controllers/home/homeController.js
@@ -1,18 +1,50 @@
-const categoryModel = require('../../models/categoryModel')
-const { responseReturn } = require("../../utiles/response")
+const categoryModel = require("../../models/categoryModel");
+const productModel = require("../../models/productModel");
+const { responseReturn } = require("../../utiles/response");
+class homeController {
+  formateProduct = () => {};
 
-class homeController{
-    get_categorys = async(req,res) => {
-        try {
-            const categorys = await categoryModel.find({})
-            responseReturn(res,200, {
-                categorys
-            })
-            
-        } catch (error) {
-            console.log(error.message)
-        }
+  
+  get_categorys = async (req, res) => {
+    try {
+      const categorys = await categoryModel.find({});
+      responseReturn(res, 200, {
+        categorys,
+      });
+    } catch (error) {
+      console.log(error.message);
     }
+  };
+  //end method
+
+  get_products = async (req, res) => {
+    try {
+      const products = await productModel.find({}).limit(12).sort({
+        createdAt: -1,
+      });
+      const allProduct1 = await productModel.find({}).limit(9).sort({
+        createdAt: -1,
+      });
+      const latest_product = this.formateProduct(allProduct1);
+      const allProduct2 = await productModel.find({}).limit(9).sort({
+        rating: -1,
+      });
+      const topRated_product = this.formateProduct(allProduct2);
+      const allProduct3 = await productModel.find({}).limit(9).sort({
+        discount: -1,
+      });
+      const discount_product = this.formateProduct(allProduct3);
+      responseReturn(res, 200, {
+        products,
+        latest_product,
+        topRated_product,
+        discount_product,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  //end method
 }
 
-module.exports = new homeController()
+module.exports = new homeController();
--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -2,6 +2,7 @@ const homeController = require("../../controllers/home/homeController");
 const router = require("express").Router();
 
 router.get("/get-categorys", homeController.get_categorys);
+router.get("/get-products", homeController.get_products);
 
 
 
