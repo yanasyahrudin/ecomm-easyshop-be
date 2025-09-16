916fa2f7ba64c6d8c1d5680df5e786989e4ffc31
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-16 14:31:05 +0700

    customer register setup part 3

--- controllers/home/customerAuthController.js
+++ controllers/home/customerAuthController.js
@@ -1,7 +1,31 @@
+const customerModel = require("../../models/customerModel");
+const { responseReturn } = require("../../utils/response");
+const brypt = require("bcrypt");
+const sellerCustomerModel = require("../../models/sellerCustomerModel");
+
 class customerAuthController {
-    customer_register(req, res) {
-        console.log(req.body)   
+  customer_register = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+      const customer = await customerModel.findOne({ email });
+      if (customer) {
+        responseReturn(res, 404, { error: "Email already exists" });
+      } else {
+        const createCustomer = await customerModel({
+          name: name.trim(),
+          email: email.trim(),
+          password: await brypt.hash(password, 10),
+          method: "manually",
+        });
+        await sellerCustomerModel.create({
+          myId: createCustomer.id,
+        });
+      }
+    } catch (error) {
+      
     }
+  };
+  //end method
 }
 
-module.exports = new customerAuthController()
+module.exports = new customerAuthController();
