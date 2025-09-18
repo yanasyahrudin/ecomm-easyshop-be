ed61515522499c6733572462ec341b651b95169b
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-18 16:33:44 +0700

    customer register setup part 4

--- controllers/home/customerAuthController.js
+++ controllers/home/customerAuthController.js
@@ -1,8 +1,8 @@
 const customerModel = require("../../models/customerModel");
-const { responseReturn } = require("../../utils/response");
+const { responseReturn } = require("../../utiles/response");
 const brypt = require("bcrypt");
-const sellerCustomerModel = require("../../models/sellerCustomerModel");
-
+const sellerCustomerModel = require("../../models/chat/sellerCustomerModel");
+const { createToken } = require("../../utiles/tokenCreate");
 class customerAuthController {
   customer_register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -11,7 +11,7 @@ class customerAuthController {
       if (customer) {
         responseReturn(res, 404, { error: "Email already exists" });
       } else {
-        const createCustomer = await customerModel({
+        const createCustomer = await customerModel.create({
           name: name.trim(),
           email: email.trim(),
           password: await brypt.hash(password, 10),
@@ -20,9 +20,22 @@ class customerAuthController {
         await sellerCustomerModel.create({
           myId: createCustomer.id,
         });
+        const token = await createToken({
+          id: createCustomer.id,
+          name: createCustomer.name,
+          email: createCustomer.email,
+          method: createCustomer.method,
+        });
+        res.cookie("customerToken", token, {
+          expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        });
+        responseReturn(res, 201, {
+          message: "User Register Successfully",
+          token,
+        });
       }
     } catch (error) {
-      
+      console.log(error.message);
     }
   };
   //end method
