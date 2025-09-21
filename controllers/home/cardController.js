c1e52e3a21e44b2cbe35d6f3d4003fd561097fe7
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-21 19:11:10 +0700

    card product get and show part 6

--- controllers/home/cardController.js
+++ controllers/home/cardController.js
@@ -1,6 +1,8 @@
 const cardModel = require("../../models/cardModel");
-const {responseReturn} = require("../../utiles/response");
-const {mongo: {ObjectId}} = require("mongoose");
+const { responseReturn } = require("../../utiles/response");
+const {
+  mongo: { ObjectId },
+} = require("mongoose");
 class cardController {
   add_to_card = async (req, res) => {
     const { userId, productId, quantity } = req.body;
@@ -40,8 +42,9 @@ class cardController {
   //end method
 
   get_card_product = async (req, res) => {
+    const co = 5;
     const { userId } = req.params;
-    
+
     try {
       const card_products = await cardModel.aggregate([
         {
@@ -58,33 +61,71 @@ class cardController {
           },
         },
       ]);
-      let buy_product_item = 0
-      let calculatePrice = 0
-      let card_product_count = 0
-      const outOfStockProduct = card_products.filter((p) => p.products[0].stock < p.quantity);
-      for(let i=0; i < outOfStockProduct.length; i++){
-        card_product_count = card_product_count + outOfStockProduct[i].quantity
+      let buy_product_item = 0;
+      let calculatePrice = 0;
+      let card_product_count = 0;
+      const outOfStockProduct = card_products.filter(
+        (p) => p.products[0].stock < p.quantity
+      );
+      for (let i = 0; i < outOfStockProduct.length; i++) {
+        card_product_count = card_product_count + outOfStockProduct[i].quantity;
       }
-      const stockProduct = card_products.filter((p) => p.products[0].stock >= p.quantity);
-      for(let i=0; i < stockProduct.length; i++){
-        const {quantity} = stockProduct[i]
-        card_product_count = card_product_count + quantity
-        buy_product_item = buy_product_item + quantity
+      const stockProduct = card_products.filter(
+        (p) => p.products[0].stock >= p.quantity
+      );
+      for (let i = 0; i < stockProduct.length; i++) {
+        const { quantity } = stockProduct[i];
+        card_product_count = card_product_count + quantity;
+        buy_product_item = buy_product_item + quantity;
         const { price, discount } = stockProduct[i].products[0];
-        if (discount !== 0 ) {
-          calculatePrice = calculatePrice + quantity * (price - Math.floor((price * discount) / 100))
+        if (discount !== 0) {
+          calculatePrice =
+            calculatePrice +
+            quantity * (price - Math.floor((price * discount) / 100));
         } else {
-          calculatePrice = calculatePrice + quantity * price
+          calculatePrice = calculatePrice + quantity * price;
         }
-      }//end for
-      let p = []
+      } //end for
+      let p = [];
+      let unique = [
+        ...new Set(stockProduct.map((p) => p.products[0].sellerId.toString())), 
+      ];
+      for (let i = 0; i < unique.length; i++) {
+        let price = 0;
+        for (let j = 0; j < stockProduct.length; j++) {
+          const tempProduct = stockProduct[j].products[0];
+          if (unique[i] === tempProduct.sellerId.toString()) {
+            let pri = 0;
+            if (tempProduct.discount !== 0) {
+              pri = tempProduct.price - Math.floor((tempProduct.price * tempProduct.discount) / 100);
+            } else {
+              pri = tempProduct.price;
+            }
+            pri = pri - Math.floor((pri * co) / 100);
+            price = price + pri * stockProduct[j].quantity;
+            p[i] = {
+              sellerId: unique[i],
+              shopName: tempProduct.shopName,
+              price,
+              products: p[i] ? [...p[i].products, {
+                _id: stockProduct[j]._id,
+                quantity: stockProduct[j].quantity,
+                productInfo: tempProduct,
+              }] : [{
+                _id: stockProduct[j]._id,
+                quantity: stockProduct[j].quantity,
+                productInfo: tempProduct,}],
+            };
+          }
+        }
+      } 
       
-      {console.log(card_products);}
+      console.log(p);
 
     } catch (error) {
       console.log(error.message);
     }
-  }
+  }; //end method
 }
 
 module.exports = new cardController();
