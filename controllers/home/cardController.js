099eaf32d59fa2c1f88e2bd46bdc812df5db3863
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-21 13:39:49 +0700

    card product get and show part 5

--- controllers/home/cardController.js
+++ controllers/home/cardController.js
@@ -49,8 +49,37 @@ class cardController {
             userId: new Object(userId),
           },
         },
+        {
+          $lookup: {
+            from: "products",
+            localField: "productId",
+            foreignField: "_id",
+            as: "products",
+          },
+        },
       ]);
-      console.log(card_products);
+      let buy_product_item = 0
+      let calculatePrice = 0
+      let card_product_count = 0
+      const outOfStockProduct = card_products.filter((p) => p.products[0].stock < p.quantity);
+      for(let i=0; i < outOfStockProduct.length; i++){
+        card_product_count = card_product_count + outOfStockProduct[i].quantity
+      }
+      const stockProduct = card_products.filter((p) => p.products[0].stock >= p.quantity);
+      for(let i=0; i < stockProduct.length; i++){
+        const {quantity} = stockProduct[i]
+        card_product_count = card_product_count + quantity
+        buy_product_item = buy_product_item + quantity
+        const { price, discount } = stockProduct[i].products[0];
+        if (discount !== 0 ) {
+          calculatePrice = calculatePrice + quantity * (price - Math.floor((price * discount) / 100))
+        } else {
+          calculatePrice = calculatePrice + quantity * price
+        }
+      }//end for
+      let p = []
+      
+      {console.log(card_products);}
 
     } catch (error) {
       console.log(error.message);
