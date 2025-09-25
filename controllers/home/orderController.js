0a93532093e6993348f0011c5fde2b52e9bf042e
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-09-25 19:00:45 +0700

    customer order place part 4

--- controllers/home/orderController.js
+++ controllers/home/orderController.js
@@ -1,9 +1,13 @@
-const cardModel = require("../../models/cardModel");
-const { responseReturn } = require("../../utiles/response");
-
+const authOrder = require("../../models/authOrder");
+const customerOrder = require("../../models/customerOrder");
+const moment = require("moment");
 class orderController {
   place_order = async (req, res) => {
-    console.log(req.body);
+    const {price, products, shipping_fee, shoppingInfo, userId,} = req.body;
+    let authorOrderData = []
+    let cardId = []
+    const tempDate = moment(Date.now()).format("LLL");
+    console.log(tempDate);
     try {
     } catch (error) {
       console.log(error.message);
---
+++ models/authOrder.js
@@ -0,0 +1,43 @@
+/** @format */
+
+const { Schema, model } = require("mongoose");
+
+const authSchema = new Schema(
+  {
+    orderId: {
+      type: Schema.ObjectId,
+      required: true,
+    },
+    sellerId: {
+      type: Schema.ObjectId,
+      required: true,
+    },
+    products: {
+      type: Array,
+      required: true,
+    },
+    price: {
+      type: Number,
+        required: true,
+    },
+    payment_status: {
+      type: String,
+        required: true,
+    },
+    shippingInfo: {
+      type: Object,
+        required: true,
+    },
+    delivery_status: {
+      type: String,
+        required: true,
+    },
+    date: {
+        type: string,
+        required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("authOrders", authSchema);
