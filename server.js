1758b6607306eddca861885d33f2555e2a811c96
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-10-25 15:23:40 +0700

    customer to seller chat system part 2

--- server.js
+++ server.js
@@ -6,13 +6,24 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const { dbConnect } = require('./utiles/db')
 
+const socket = require('socket.io')
+const http = require('http')
+const server = http.createServer(app)
+
 app.use(cors({
     origin: ['http://localhost:3000'],
     credentials: true
 }))
+
+const io = socket(server, {
+    cors: {
+        origin: '*',
+        credentials: true
+    }
+})
+
 app.use(bodyParser.json())
 app.use(cookieParser())
-
 
 app.use('/api/home', require('./routes/home/homeRoutes'))
 app.use('/api', require('./routes/authRoutes'))
@@ -26,4 +37,4 @@ app.use('/api', require('./routes/home/customerAuthRoutes'))
 app.get('/', (req, res)=>res.send('My backend'))
 const port = process.env.PORT
 dbConnect()
-app.listen(port, ()=> console.log(` Server is running on port ${port}`))
+server.listen(port, ()=> console.log(` Server is running on port ${port}`))
