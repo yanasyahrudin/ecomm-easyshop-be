a505eb9c4b06b3eb42935da1a57eae6bae17c0e3
yanasyahrudin <11207252@nusamandiri.ac.id>
2025-10-26 13:26:14 +0700

    customer to seller chat system part 3

--- server.js
+++ server.js
@@ -1,40 +1,57 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const { dbConnect } = require('./utiles/db')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { dbConnect } = require("./utiles/db");
 
-const socket = require('socket.io')
-const http = require('http')
-const server = http.createServer(app)
+const socket = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
 
-app.use(cors({
-    origin: ['http://localhost:3000'],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 const io = socket(server, {
-    cors: {
-        origin: '*',
-        credentials: true
-    }
-})
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
 
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.use('/api/home', require('./routes/home/homeRoutes'))
-app.use('/api', require('./routes/authRoutes'))
-app.use('/api', require('./routes/home/orderRoutes'))
-app.use('/api', require('./routes/home/cardRoutes'))
-app.use('/api', require('./routes/dashboard/categoryRoutes'))
-app.use('/api', require('./routes/dashboard/productRoutes'))
-app.use('/api', require('./routes/dashboard/sellerRoutes'))
-app.use('/api', require('./routes/home/customerAuthRoutes'))
+app.use("/api/home", require("./routes/home/homeRoutes"));
+app.use("/api", require("./routes/authRoutes"));
+app.use("/api", require("./routes/home/orderRoutes"));
+app.use("/api", require("./routes/home/cardRoutes"));
+app.use("/api", require("./routes/dashboard/categoryRoutes"));
+app.use("/api", require("./routes/dashboard/productRoutes"));
+app.use("/api", require("./routes/dashboard/sellerRoutes"));
+app.use("/api", require("./routes/home/customerAuthRoutes"));
 
-app.get('/', (req, res)=>res.send('My backend'))
-const port = process.env.PORT
-dbConnect()
-server.listen(port, ()=> console.log(` Server is running on port ${port}`))
+app.get("/", (req, res) => res.send("My backend"));
+const port = process.env.PORT;
+dbConnect();
+server.listen(port, () => console.log(` Server is running on port ${port}`));
+
+var allCustomer = [];
+const addUser = (customerId, socketId, userInfo) => {
+  const checkUser = allCustomer.some((u) => u.customerId === customerId);
+  if (!checkUser) {
+    allCustomer.push({ customerId, socketId, userInfo });
+  }
+};
+
+io.on("connection", (soc) => {
+  console.log("socket server running...");
+  soc.on("add_user", (customerId, userInfo) => {
+    addUser(customerId, soc.id, userInfo);
+  });
+});
